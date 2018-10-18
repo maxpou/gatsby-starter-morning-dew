3b2697ecfa2a9806b473dfc182ba167ace34fffb
Maxence POUTORD <maxence.poutord@gmail.com>
2018-10-18 22:02:12 +0100

    #5 metatags for tags page

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,30 +1,23 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
-import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
 import PostsList from '../components/PostsList/PostsList'
 import Wrapper from '../components/Wrapper/Wrapper'
+import SEO from '../components/SEO/SEO';
+import Hero from '../components/Hero/Hero';
 
 class Tags extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const pageTitle = `#${this.props.pageContext.tag}`
-
-    const siteDescription = get(
-      this,
-      'props.data.site.siteMetadata.description'
-    )
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
 
     return (
       <Layout location={this.props.location} title={pageTitle}>
-        <Helmet
-          htmlAttributes={{ lang: 'en' }}
-          meta={[{ name: 'description', content: siteDescription }]}
-          title={`${pageTitle} | ${siteTitle}`}
-        />
+        <SEO title={pageTitle} />
+        <Hero title={pageTitle} />
+
         <Wrapper>
           <h1>Posts tagged as "{this.props.pageContext.tag}"</h1>
           <PostsList posts={posts} />
@@ -38,12 +31,6 @@ export default Tags
 
 export const pageQuery = graphql`
   query PostsByTag($tag: String!) {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
     allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
       filter: { frontmatter: { tags: { eq: $tag } } }
