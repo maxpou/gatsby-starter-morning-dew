b1a2e8a179662e3a3c4d0227c8a36e3a3468a648
Maxence Poutord <maxence.poutord@gmail.com>
2019-11-02 15:02:11 +0700

    fix: improve a11y for emojis (#88)

--- src/pages/404.js
+++ src/pages/404.js
@@ -14,7 +14,8 @@ const MainTitle = styled.h1`
   font-size: 3rem;
 `
 
-const Ghost = styled.p`
+const Ghost = styled.span`
+  display: block;
   line-height: 1.5;
   text-align: center;
   font-size: 7rem;
@@ -58,7 +59,7 @@ const NotFoundPage = props => {
       <SEO title="Page Not Found" />
       <Wrapper>
         <MainTitle>404 Page Not Found</MainTitle>
-        <Ghost>👻</Ghost>
+        <Ghost role="img" aria-label="Ghost">👻</Ghost>
         <Text>
           Looks like you've followed a broken link or entered a URL that doesn't
           exist on this site.
