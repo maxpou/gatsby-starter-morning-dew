d9c7432f69e332d0dc268f9dc2fcbf191ad63b58
Maxence POUTORD <maxence.poutord@gmail.com>
2020-01-14 21:14:06 +0000

    docs: fix typos in authorDescription

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -7,8 +7,8 @@ module.exports = {
   multilangPosts: true, // enable/disable flags in post lists
   authorDescription: `
   For the last decade, Maxence Poutord has worked with a variety of web technologies. He is currently focused on front-end development.
-  On his day to day job, he is working as a senior front-end engineer at VSware. He is also a frequent tech speaker and a mentor.
-  As a new digital nomad, he is living where the WIFI and sun is 😎 <br>
+  On his day to day job, he is working as a senior front-end engineer at VSware. He is also an occasional tech speaker and a mentor.
+  As a digital nomad, he is living where the WiFi and sun are 😎 <br>
   Do you want to know more? <a href="https://www.maxpou.fr/about" target="_blank">Visit my website!</a>
   `,
   siteUrl: 'https://maxpou.github.io/',
