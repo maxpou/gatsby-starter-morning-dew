c3d22f74accc518f653aaf5fb52c91ae6cbe5a57
Maxence POUTORD <maxence.poutord@gmail.com>
2020-01-15 19:36:05 +0000

    fix(tagPage): add missing timeToRead + small changes

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import styled from 'styled-components'
 import get from 'lodash/get'
 
 import Layout from '../components/layout'
@@ -8,18 +9,22 @@ import Wrapper from '../components/Wrapper'
 import SEO from '../components/SEO'
 import Hero from '../components/Hero'
 
+const PageTitle = styled.h1`
+  padding-bottom: 10px;
+`
+
 class Tags extends React.Component {
   render() {
     const pageTitle = `#${this.props.pageContext.tag}`
     const posts = get(this, 'props.data.posts.edges')
 
     return (
-      <Layout location={this.props.location} title={pageTitle}>
-        <SEO title={pageTitle} />
+      <Layout location={this.props.location}>
+        <SEO title={`Top blog posts on ${this.props.pageContext.tag}`} />
         <Hero title={pageTitle} />
 
         <Wrapper>
-          <h1>Posts tagged as "{this.props.pageContext.tag}"</h1>
+          <PageTitle>Posts tagged as {this.props.pageContext.tag}</PageTitle>
           <PostsList posts={posts} />
         </Wrapper>
       </Layout>
@@ -38,8 +43,8 @@ export const pageQuery = graphql`
       edges {
         node {
           excerpt
+          timeToRead
           frontmatter {
-            date(formatString: "DD MMMM, YYYY")
             title
             tags
             language
