96e6b941c1231fae030c2456e99a14c9dc42ab59
David A. Hannasch <David.A.Hannasch@gmail.com>
2020-05-03 13:16:43 -0600

    Including gatsby-remark-embed-video in gatsby-plugin-mdx makes videos work!

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,23 @@ module.exports = {
         },
         gatsbyRemarkPlugins: [
           {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+              urlOverrides: [
+                {
+                  id: 'youtube',
+                  embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
+                }
+              ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+              containerClass: 'embedVideo-container', //Optional: Custom CSS class for iframe container
+            }
+          },
+          {
             // gatsby-remark-embed-video still does not work if gatsby-remark-images is removed here.
             resolve: 'gatsby-remark-images',
             options: {
