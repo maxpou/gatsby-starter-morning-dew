35843cd5fba354ff54255303f9ff876c33095915
Maxence POUTORD <maxence.poutord@gmail.com>
2020-05-04 11:25:16 +0200

    fix: dependencies can't end with two zeros

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
-import get from 'lodash/get'
 
 import Layout from '../components/layout'
 import PostsList from '../components/PostsList'
