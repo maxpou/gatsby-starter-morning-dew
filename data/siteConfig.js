0e0d7cbf867dc2fb9d3c1afca17d415e96beb0f8
Maxence POUTORD <maxence.poutord@gmail.com>
2020-06-01 17:07:08 +0200

    docs: add noopener on external link

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -9,7 +9,7 @@ module.exports = {
   For the last decade, Maxence Poutord has worked with a variety of web technologies. He is currently focused on front-end development.
   On his day to day job, he is working as a senior front-end engineer at VSware. He is also an occasional tech speaker and a mentor.
   As a digital nomad, he is living where the WiFi and sun are 😎 <br>
-  Do you want to know more? <a href="https://www.maxpou.fr/about" target="_blank">Visit my website!</a>
+  Do you want to know more? <a href="https://www.maxpou.fr/about" rel="noopener" target="_blank">Visit my website!</a>
   `,
   siteUrl: 'https://maxpou.github.io/',
   disqusSiteUrl: 'https://www.maxpou.fr/',
