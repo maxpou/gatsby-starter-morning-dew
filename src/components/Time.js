f1acde094ec931e4aaca8f9c18788b6307e428d2
Maxence POUTORD <maxence.poutord@gmail.com>
2020-07-01 18:05:39 +0200

    feat(seo): add structured data with json-ld
    fix: remove react warning (missing keys)
    fix: remove styled-component dynamically created (see https://github.com/styled-components/styled-components/issues/3117)
    chore: bump multiple dependencies
    refactor: run prettier

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const TimeContainer = styled.time`
+  color: var(--color-textSecondary);
+`
+
 const Time = props => {
-  const TimeContainer = styled.time`
-    color: var(--color-textSecondary);
-  `
   const { date } = props
 
   const dateObject = new Date(date)
